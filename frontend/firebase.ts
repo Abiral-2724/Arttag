88a5cf9e946ebb82fddf11d7a0badda620cffe55
Abiral-2724 <abiral2004jain@gmail.com>
2025-10-24 15:23:05 +0530

    secret variable updated

--- frontend/firebase.ts
+++ frontend/firebase.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA35DmXM_wi3HzwQ-04etkw4ElXuOjJU6c",
-  authDomain: "arttag-login-new.firebaseapp.com",
-  projectId: "arttag-login-new",
-  storageBucket: "arttag-login-new.firebasestorage.app",
-  messagingSenderId: "776791259974",
-  appId: "1:776791259974:web:ab9c24d7a674ba839f8347",
-  measurementId: "G-JDER0WMTKN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
