2ae1956d16f56cd2a7773276321e69ea1bd2f09a
Abiral-2724 <abiral2004jain@gmail.com>
2025-12-11 22:15:47 +0530

    frontend api added

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json()) ;
 app.use(express.urlencoded({extended : true})) ; 
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://ecommerce-two-teal-40.vercel.app',
     credentials: true, 
 };
 
